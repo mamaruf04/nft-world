0f1e0aade1be977465ad73ee47167ca9c9da0145
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-03-26 22:04:24 +0600

    product added

--- src/Shop/Shop.js
+++ src/Shop/Shop.js
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Shop = () => {
+    const[product,setProduct]= useState([])
+
+    useEffect(() =>{
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data=> console.log(data))
+    },[])
     return (
         <div>
             <div>
