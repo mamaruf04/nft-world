11110e7b62815581d04934b6c26d6bdaa0651a88
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-03-26 21:45:46 +0600

    initial-commit

---
+++ src/Shop/Shop.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            <div>
+                <h1>product section</h1>
+            </div>
+            <div>
+                <h1>cart section</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
