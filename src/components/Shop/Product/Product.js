c6c1a92658a2dccafb23cef8ba4bbcc3df3dd24a
M A Maruf <90461183+mamaruf04@users.noreply.github.com>
2022-03-27 21:40:02 +0600

    Update Product.js

--- src/components/Shop/Product/Product.js
+++ src/components/Shop/Product/Product.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
     const {Product, handelAddToCard}= props;
-    const{img, name, price, seller, ratings}= Product;
+    const{img, name, price}= Product;
     return (
         <div id='product'>
             <img className='product-img' src={img} alt="" />
             <div className='product-info'>
                 <p className='product-name'>{name}</p>
                 <p>Price: ${price}</p>
-                <p><small>Seller: {seller}</small></p>
-                <p><small>Ratings: {ratings} stars</small></p>
             </div>
             <button onClick={() =>handelAddToCard(Product)} className='btn-card'>
                 <p className='btn-text'>Add to Cart <i className="fa fa-shopping-cart"></i></p>
